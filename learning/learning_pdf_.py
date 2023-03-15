03f63517c3598122c39f3c3e9cac52f7d1eb7689
Matthew.Herring <matthew.herring@geoscipy.com>
2023-03-15 15:00:13 -0500

    Synchronizing with GItHub

--- learning/learning_pdf_.py
+++ learning/learning_pdf_.py
@@ -1,1 +1,11 @@
+# Import Area
 import pypdf
+import os 
+
+# Variable Area
+workingfolder = r'C:\Dev_Source\github_repos\GeoSciGuy\learning\pdf_files'
+pdf_in_fn = r'dr013123.pdf'
+
+pdfFileObj = open((os.path.join(workingfolder, pdf_in_fn)),'rb')
+pdfReader = pypdf.PdfFileReader(pdfFileObj)
+
