d300adfeeab00f578b8873906f459f9618f6d169
Geo SciGuy <102614848+GeoSciGuy@users.noreply.github.com>
2025-10-08 22:05:47 -0500

    Create template.py
    Creating a Template to be used with Python Development.

---
+++ templates/template.py
@@ -0,0 +1,22 @@
+# Created by : Matthew Herrinng
+# Created on : 2025-10-07   
+# Description: Template for Python files
+#              (c) Copyright 2025 Matthew Herrinng
+#              All rights reserved.
+
+
+# Import Area
+
+# Variables Area
+
+# Helper FUnctions
+
+# Main Function
+def main():
+    try:
+        print("Hello, World!")
+    except Exception as e:
+        print("An error occurred:", e)
+
+
+main()
